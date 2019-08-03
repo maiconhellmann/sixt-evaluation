bb2a5163c248359cd8d1fa295d4dfced9f39a924
Maicon Hellmann <maiconhellmann@gmail.com>
2019-08-03 17:05:44 +0200

    mapView setup

--- app/src/main/java/com/maiconhellmann/sixtcodechallenge/feature/car/CarMapFragment.kt
+++ app/src/main/java/com/maiconhellmann/sixtcodechallenge/feature/car/CarMapFragment.kt
@@ -8,6 +8,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 import com.maiconhellmann.sixtcodechallenge.R
 import com.maiconhellmann.sixtcodechallenge.databinding.CarMapFragmentBinding
 import com.maiconhellmann.sixtcodechallenge.feature.list.CarListFragment
@@ -22,7 +26,11 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
  * Created by maiconhellmann on 03/08/2019
  * 
  * (c) 2019 
- */class CarMapFragment : Fragment() {
+ */class CarMapFragment : Fragment(), OnMapReadyCallback {
+
+    private val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+    private var mMap: GoogleMap?= null
     private lateinit var binding: CarMapFragmentBinding
     private val viewModel: CarListViewModel by sharedViewModel()
 
@@ -40,6 +48,16 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
         binding.lifecycleOwner = this
 
         setupViewModel()
+
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY)
+        }
+
+        with(binding.mapView) {
+            onCreate(mapViewBundle)
+            getMapAsync(this@CarMapFragment)
+        }
     }
 
     private fun setupViewModel() {
@@ -72,5 +90,71 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
     private fun showError(throwable: Throwable) {
         view?.context?.toast(throwable.toString())
         Log.e(CarListFragment::class.java.simpleName, "Error", throwable)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap
+        mMap?.apply {
+            setMinZoomPreference(12f)
+            isIndoorEnabled = true
+
+            with(uiSettings) {
+                isIndoorLevelPickerEnabled = false
+                isMyLocationButtonEnabled = true
+                isMapToolbarEnabled = true
+                isCompassEnabled = true
+                isZoomControlsEnabled = false
+                isRotateGesturesEnabled = true
+                isScrollGesturesEnabled = true
+                isTiltGesturesEnabled = false
+                isZoomGesturesEnabled = true
+            }
+
+            //TODO remove fixed point: new york
+            val ny = LatLng(40.7143528, -74.0059731)
+            moveCamera(CameraUpdateFactory.newLatLng(ny))
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        var mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle)
+        }
+
+        binding.mapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onPause() {
+        binding.mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        binding.mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
